a344ad295644a8fe9a58c5049fd5b89adc058901
taco boi <tylerchall16@gmail.com>
2023-07-31 18:59:27 -0500

    refactoring in progress

--- app/api/movies/route.tsx
+++ app/api/movies/route.tsx
@@ -1,19 +1,17 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  try {
-    const res = await fetch(
-      `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
-    );
-    if (!res.ok) {
-      console.log("Failed to fetch data in getMovies");
-    } else {
-      console.log("Fetch was successful!");
-    }
+  const res = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
+  );
 
-    return NextResponse.json(res);
-  } catch (err: unknown) {
-    console.error("error", err);
-    return []; // Return an empty array in case of an error or failed fetch
+  if (!res.ok) {
+    console.log("Fetching failed");
+  } else {
+    console.log("Fetching was successful!");
   }
+
+  const movies = await res.json();
+
+  return NextResponse.json(movies);
 }
