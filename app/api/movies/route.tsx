bd51ca815b1a0683327750f84c40a86c0cb83841
taco boi <tylerchall16@gmail.com>
2023-08-14 00:53:53 -0500

    changed api variable name

--- app/api/movies/route.tsx
+++ app/api/movies/route.tsx
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   const res = await fetch(
-    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`,
+    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.NEXT_APP_API_KEY}`,
     {
       cache: "no-cache",
     }
