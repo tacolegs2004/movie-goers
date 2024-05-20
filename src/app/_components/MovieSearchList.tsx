24dda664719a95a327c1a9b171c4db01d73a965d
taco boi <tylerchall16@gmail.com>
2024-05-19 22:36:56 -0500

    updated packages

--- src/app/_components/MovieSearchList.tsx
+++ src/app/_components/MovieSearchList.tsx
@@ -1,21 +1,21 @@
-import { TSearchedMovieObject } from "@/src/lib/types/MovieTypes";
+import { TMovie } from "@/src/lib/types/MovieTypes";
 import { use } from "react";
 import MovieListCard from "./MovieListCard";
 
 export default function MovieSearchList({
   moviePromise,
 }: {
-  moviePromise: Promise<TSearchedMovieObject>;
+  moviePromise: Promise<TMovie>;
 }) {
-  const { results } = use(moviePromise) as TSearchedMovieObject;
-
+  const results = use(moviePromise) as TMovie;
+  results.results.map((result) => console.log(result));
   if (!results) {
     return <NullResults />;
   }
   return (
     <div className="flex flex-col justify-center items-center">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {results.map((result) => (
+        {results.results.map((result) => (
           <MovieListCard key={result.id} results={result} />
         ))}
       </div>
